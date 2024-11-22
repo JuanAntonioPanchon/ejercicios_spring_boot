bab50f32c65b05ef2fb7d15c3b71bb0a3231b195
JuanAntonioPanchon <martin.panchon.juan.antonio@iescamas.es>
2024-11-22 09:05:16 +0100

    Realizado ejercicio 1 parImpar

---
+++ estructuras/src/main/java/com/ejercicio/estructuras/ControladorParImpar.java
@@ -0,0 +1,20 @@
+package com.ejercicio.estructuras;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ControladorParImpar {
+	@PostMapping("/parImpar")
+	public String parImparEjercicio1(@RequestParam int numero) {
+		String res;
+		if(numero % 2 == 0) {
+			res = "El numero " + numero + " es par";
+		}else {
+			res = "El numero " + numero + " es impar";
+		}
+		
+		return res;
+	}
+}
