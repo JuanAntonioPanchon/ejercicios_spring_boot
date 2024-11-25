b2cd3467c979bebbe9db3fbea6d81e6d4dc2ec47
Juan Antonio Martin Panchon <martin.panchon.juan.antonio@iescamas.es>
2024-11-25 19:14:28 +0100

    Realizado ejercicio 4 CalculadoraIMC

--- estructuras/src/main/java/com/ejercicio/estructuras/Controlador/ControladorEjercicios.java
+++ estructuras/src/main/java/com/ejercicio/estructuras/Controlador/ControladorEjercicios.java
@@ -32,6 +32,7 @@ public class ControladorEjercicios {
 		return res;
 	}
 	
+	//Ejercicio3 promedioCalificaciones
 	@PostMapping("/promedioCalificaciones")
 	public String promedioCalificaciones (@RequestParam String calificaciones) {
 		String nuevaCalificaciones = calificaciones.replaceAll(" ", "");
@@ -45,7 +46,23 @@ public class ControladorEjercicios {
 		
 		media = nota / notas.length;
 		return media > 5 ? "Alumno tiene una media de " + media + " por tanto está aprobado " : " Alumno tiene media de " + media + " por tanto está suspenso";
-		
-		
 	}
+	
+		//Ejercicio4 calculadoraIMC
+			@PostMapping("/calcularIMC")
+			public String calculadora_imc(@RequestParam double peso, @RequestParam double altura) {
+				String res = "";
+				double imc = peso / (altura * altura);
+				if (imc < 18.5) {
+					res = "IMC = " + imc + " Bajo peso";
+				}else if(imc >= 18.5 && imc <= 24.9) {
+					res = "IMC = " + imc +  " Peso Normal";
+				}else if(imc >=25 && imc <= 29.9) {
+					res = "IMC = " + imc +  "Sobrepeso";
+				}else {
+					res = "IMC = " + imc + " Obesidad";
+				}
+				
+				return res;
+			}
 }
