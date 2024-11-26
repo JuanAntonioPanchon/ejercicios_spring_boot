7196187f51a9386a15536fe1e2e09d7103b2f2b6
Juan Antonio Martin Panchon <martin.panchon.juan.antonio@iescamas.es>
2024-11-26 17:55:43 +0100

    Realizado ejercicio 5 happy path porque si se pulsa el boton sin puntuar salta error

--- estructuras/src/main/java/com/ejercicio/estructuras/Controlador/ControladorEjercicios.java
+++ estructuras/src/main/java/com/ejercicio/estructuras/Controlador/ControladorEjercicios.java
@@ -9,19 +9,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ControladorEjercicios {
-	//Ejercicio1 parImpar
+	// Ejercicio1 parImpar
 	@PostMapping("/parImpar")
 	public String parImparEjercicio1(@RequestParam int numero) {
 		String res;
-		if(numero % 2 == 0) {
+		if (numero % 2 == 0) {
 			res = "El numero " + numero + " es par";
-		}else {
+		} else {
 			res = "El numero " + numero + " es impar";
 		}
-		return "<h1>" +  res + "</h1>";
+		return "<h1>" + res + "</h1>";
 	}
-	
-	//Ejercicio2 tablaMultiplicar
+
+	// Ejercicio2 tablaMultiplicar
 	@PostMapping("/tablaMultiplicar")
 	public String tablaMultiplicarEjercicio2(@RequestParam int numero) {
 		String res = "";
@@ -31,38 +31,74 @@ public class ControladorEjercicios {
 		}
 		return res;
 	}
-	
-	//Ejercicio3 promedioCalificaciones
+
+	// Ejercicio3 promedioCalificaciones
 	@PostMapping("/promedioCalificaciones")
-	public String promedioCalificaciones (@RequestParam String calificaciones) {
+	public String promedioCalificaciones(@RequestParam String calificaciones) {
 		String nuevaCalificaciones = calificaciones.replaceAll(" ", "");
 		String[] notas = nuevaCalificaciones.split(",");
 		double nota = 0;
 		double media = 0;
-		
+
 		for (int i = 0; i < notas.length; i++) {
 			nota += Double.parseDouble(notas[i]);
 		}
-		
+
 		media = nota / notas.length;
-		return media > 5 ? "Alumno tiene una media de " + media + " por tanto está aprobado " : " Alumno tiene media de " + media + " por tanto está suspenso";
+		return media > 5 ? "Alumno tiene una media de " + media + " por tanto está aprobado "
+				: " Alumno tiene media de " + media + " por tanto está suspenso";
 	}
-	
-		//Ejercicio4 calculadoraIMC
-			@PostMapping("/calcularIMC")
-			public String calculadora_imc(@RequestParam double peso, @RequestParam double altura) {
-				String res = "";
-				double imc = peso / (altura * altura);
-				if (imc < 18.5) {
-					res = "IMC = " + imc + " Bajo peso";
-				}else if(imc >= 18.5 && imc <= 24.9) {
-					res = "IMC = " + imc +  " Peso Normal";
-				}else if(imc >=25 && imc <= 29.9) {
-					res = "IMC = " + imc +  "Sobrepeso";
-				}else {
-					res = "IMC = " + imc + " Obesidad";
-				}
-				
-				return res;
+
+	// Ejercicio4 calculadoraIMC
+	@PostMapping("/calcularIMC")
+	public String calculadora_imc(@RequestParam double peso, @RequestParam double altura) {
+		String res = "";
+		double imc = peso / (altura * altura);
+		if (imc < 18.5) {
+			res = "IMC = " + imc + " Bajo peso";
+		} else if (imc >= 18.5 && imc <= 24.9) {
+			res = "IMC = " + imc + " Peso Normal";
+		} else if (imc >= 25 && imc <= 29.9) {
+			res = "IMC = " + imc + "Sobrepeso";
+		} else {
+			res = "IMC = " + imc + " Obesidad";
+		}
+
+		return res;
+	}
+
+	// Ejercicio5 encuesta satisfacción
+
+	@PostMapping("/encuesta")
+	public String encuesta(@RequestParam Integer satisfaccion) {
+		String res = "";
+		if (satisfaccion != null) {
+			switch (satisfaccion) {
+			case 1:
+				res = "Muchas gracias, su puntuación ha sido de " + satisfaccion + " trataremos de mejorar para el futuro";
+				break;
+			case 2:
+				res = "Muchas gracias, su puntuación ha sido de " + satisfaccion + " aun nos queda por mejorar";
+				break;
+			case 3:
+				res = "Muchas gracias, su puntuación ha sido de " + satisfaccion
+						+ " aunque no haya sido del todo negativo, mejoraremos para el futuro";
+				break;
+			case 4:
+				res = "Muchas gracias, su puntuación ha sido de " + satisfaccion
+						+ " siempre se puede mejorar y nosotros lo haremos para optener el máximo";
+				break;
+			case 5:
+				res = "Muchas gracias, su puntuación ha sido de " + satisfaccion
+						+ " nos alegra saber que cumplimos sus espectativas";
+				break;
+			default:
+				res = "Debes realizar una puntuación de 1 a 5, siendo 1 lo peor y 5 lo mejor";
+				break;
 			}
+		} else {
+			res = "Debes realizar una puntuacion de manera obligatoria";
+		}
+		return res;
+	}
 }
